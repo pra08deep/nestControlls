952f69538d4f2b41a759cba3e6f66c4d90b69e5a
pra08deep <pradeepmaharjan8@gmail.com>
2023-07-23 17:45:41 +0545

    Post (express Req and Res)tested by postman

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Request,Response } from 'express';
 
 @Controller('users')
 export class UsersController {
@@ -9,6 +10,38 @@ export class UsersController {
             username:'name',email:'email'
         };
 
+    }
+    
+
+    @Get('post')
+    getUserspost()
+    {
+        return { 
+            username:'name',email:'email',
+            post:[{
+                id:'1',
+                name:'name'
+        }]
+        };
+
+    }
+
+    @Get('post/commets')
+    getUserspostcomments()
+    {
+        return { 
+            username:'coometns' 
+        };
+
+    }
+
+    @Post()
+    createUser(@Req() request:Request,@Res() response:Response){
+        console.log(
+            request.body
+        );
+        response.send('created');
+        
     }
 
 }
